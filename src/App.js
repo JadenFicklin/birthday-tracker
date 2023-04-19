74cb5fb7b327613e5dc079a660a878cef3bed920
JadenFicklin <jadendficklin@gmail.com>
2023-04-18 21:45:13 -0600

    google sign in auth

--- src/App.js
+++ src/App.js
@@ -3,13 +3,45 @@ import './App.css';
 import { database } from './firebase';
 import { set, ref, onValue, push } from 'firebase/database';
 import { uid } from 'uid';
+import {
+    getAuth,
+    signInWithPopup,
+    signOut,
+    GoogleAuthProvider
+} from 'firebase/auth';
 
 function App() {
     const [name, setName] = useState('');
     const [date, setDate] = useState('');
     const [priority, setPriority] = useState('');
+    const [user, setUser] = useState('');
+    const [userUid, setUserUid] = useState('');
 
     const [arrayOfBirthdays, setArrayOfBirthdays] = useState([]);
+
+    const provider = new GoogleAuthProvider();
+    const auth = getAuth();
+
+    const handleSignIn = async () => {
+        try {
+            const result = await signInWithPopup(auth, provider);
+            const user = result.user;
+            setUserUid(user.uid);
+            setUser(user);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    const handleSignOut = async () => {
+        try {
+            await signOut(auth);
+            console.log('Signed out');
+            setUser(null);
+        } catch (error) {
+            console.log(error, 'error');
+        }
+    };
 
     const writeToDatabase = (e) => {
         e.preventDefault();
@@ -19,7 +51,8 @@ function App() {
             name,
             date,
             priority,
-            uuid
+            uuid,
+            userUid
         });
     };
 
@@ -34,37 +67,64 @@ function App() {
     }, []);
 
     return (
-        <div className="bg-gray-100 min-h-screen">
+        <div className="min-h-screen bg-gray-100">
             <h1>Birthday tracker</h1>
-            <form onSubmit={writeToDatabase}>
-                <input
-                    type="text"
-                    placeholder="name"
-                    onChange={(e) => setName(e.target.value)}
-                />
-                <input type="date" onChange={(e) => setDate(e.target.value)} />
-                <select onChange={(e) => setPriority(e.target.value)}>
-                    <option selected disabled>
-                        Select a priority color
-                    </option>
-                    <option>Red</option>
-                    <option>Yellow</option>
-                    <option>Green</option>
-                </select>
-                <input type="submit" />
-            </form>
-            <br />
-            <p>{JSON.stringify(arrayOfBirthdays)}</p>
-            {arrayOfBirthdays.map((item, key) => (
-                <div
-                    key={key}
-                    className="p-3 rounded-md shadow-xl m-3 w-32 bg-white">
-                    <p>{item.name}</p>
-                    <p>{item.date}</p>
-                    <p>{item.priority}</p>
-                    <p>{item.uuid}</p>
-                </div>
-            ))}
+            {user ? (
+                <>
+                    <h2>Logged in as {user.displayName}</h2>
+                    <img
+                        src={`https://api.allorigins.win/raw?url=${user.photoURL}`}
+                        alt=""
+                        className="w-20 h-20 rounded-full"
+                    />
+                </>
+            ) : (
+                <h2>not logged in</h2>
+            )}
+            {!user ? (
+                <button onClick={handleSignIn}>Log in</button>
+            ) : (
+                <button onClick={handleSignOut}>Log out</button>
+            )}
+            {user && (
+                <>
+                    {' '}
+                    <form onSubmit={writeToDatabase}>
+                        <input
+                            type="text"
+                            placeholder="name"
+                            onChange={(e) => setName(e.target.value)}
+                        />
+                        <input
+                            type="date"
+                            onChange={(e) => setDate(e.target.value)}
+                        />
+                        <select
+                            value=""
+                            onChange={(e) => setPriority(e.target.value)}>
+                            <option value="" disabled>
+                                Select a priority color
+                            </option>
+                            <option>Red</option>
+                            <option>Yellow</option>
+                            <option>Green</option>
+                        </select>
+                        <input type="submit" />
+                    </form>
+                    <br />
+                    {arrayOfBirthdays
+                        .filter((item) => item.userUid === userUid)
+                        .map((item, key) => (
+                            <div
+                                key={key}
+                                style={{ background: `${item.priority}` }}
+                                className="w-32 p-3 m-3 bg-white rounded-md shadow-xl">
+                                <p>{item.name}</p>
+                                <p>{item.date}</p>
+                            </div>
+                        ))}
+                </>
+            )}
         </div>
     );
 }
